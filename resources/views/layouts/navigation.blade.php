5d5b7c6c5204d6c899f0d813384b8e3b112ba184
Aydin Pramasta <aydin.pram@gmail.com>
2023-01-28 11:15:31 +0700

    add nav link to admin user

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,25 +1,36 @@
-<nav class="navbar navbar-expand-lg fixed-top bg-body-tertiary px-3">
-    <div class="container-fluid">
-        <a class="navbar-brand fs-4 fw-bold" href="{{ route('home') }}">Perpustakaan</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarItems"
-            aria-controls="navbarItems" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarItems">
-            <div class="navbar-nav ms-auto">
-                @auth
-                    <form action="{{ route('logout') }}" method="POST" onsubmit="return confirm('Anda yakin ingin keluar?')">
-                        @csrf
-                        @method('DELETE')
-                        <button class="btn btn-link nav-link" type="submit">Logout</button>
-                    </form>
-                @endauth
+<div class="fixed-top">
+    @if (in_array(auth()->user()->role, [\App\Models\User::ROLES['Admin'], \App\Models\User::ROLES['Librarian']]))
+        <div class="navbar px-5 bg-primary-subtle flex justify-content-between">
+            <span>Anda adalah <b>{{ auth()->user()->role }}</b></span>
 
-                @guest
-                    <a class="nav-link" href="{{ route('login') }}">Login</a>
-                    <a class="nav-link" href="{{ route('register') }}">Register</a>
-                @endguest
+            <a href="{{ route('admin.dashboard') }}" class="btn btn-primary">Ke Dashboard</a>
+        </div>
+    @endif
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary px-3">
+        <div class="container-fluid">
+            <a class="navbar-brand fs-4 fw-bold" href="{{ route('home') }}">Perpustakaan</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarItems"
+                aria-controls="navbarItems" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarItems">
+                <div class="navbar-nav ms-auto">
+                    @auth
+                        <form action="{{ route('logout') }}" method="POST"
+                            onsubmit="return confirm('Anda yakin ingin keluar?')">
+                            @csrf
+                            @method('DELETE')
+                            <button class="btn btn-link nav-link" type="submit">Logout</button>
+                        </form>
+                    @endauth
+
+                    @guest
+                        <a class="nav-link" href="{{ route('login') }}">Login</a>
+                        <a class="nav-link" href="{{ route('register') }}">Register</a>
+                    @endguest
+                </div>
             </div>
         </div>
-    </div>
-</nav>
+    </nav>
+</div>
