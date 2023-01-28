9ffd47c613f025a5156721b1cf9e549dfdb3ad74
Aydin Pramasta <aydin.pram@gmail.com>
2023-01-28 10:48:33 +0700

    redirect intended for login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -42,7 +42,7 @@ class AuthController extends Controller
         if (Auth::attempt($credentials, $request->boolean('remember'))) {
             $request->session()->regenerate();
 
-            return redirect()->route('home');
+            return redirect()->intended(route('home'));
         }
 
         return back()->withErrors([
